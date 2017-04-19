e52ae987e0ee2233a6b05848fe83f9695e7d29ff
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-04-19 07:54:36 +0300

    Update controller.default.js

--- app/controllers/controller.default.js
+++ app/controllers/controller.default.js
@@ -4,7 +4,6 @@
  */
 
 module
-    
     .controller('defaultCtrl', ['$scope', 'Storage', function($scope, Storage) {
 
         $scope.items = [];
@@ -18,67 +17,35 @@ module
             else {return true;}
         }
 
-        let data = Storage.getData();
+        $scope.items = Storage.getData();
        
-        if (!data || !data[0]) {
-            console.log('empty');
-// For initial loading, I  prepared the following data from two elements.
-            Storage.setData([
-                                {username: "Benjamin Franklin", 
-                                 comments: [
-                                    "Early to bed, and early to rise, Makes a man healthy, wealthy, and wise.",
-                                    "Dost thou love life? Then do not squander time, for that's the stuff life is made of.",
-                                    "No nation was ever ruined by trade",
-                                    "Trust thy self, and another shall not betray thee."
-                                    ]
-                                },
-                                {username: "Friedrich Nietzsche",
-                                 comments: [
-                                    "To forget one's purpose is the commonest form of stupidity.",
-                                    "Talking much about oneself can also be a means to conceal oneself."
-                                    ]
-                                }			
-                            ]
-                        );
-            $scope.items = Storage.getData();
-        } else {
-            console.log('full');
-            $scope.items = data;
-        }
-        
-
         $scope.clickItem = function (item) {
             $scope.textComment = '';
             $scope.disabledComment=false;
             $scope.currentIndex = $scope.items.indexOf(item);
-            return $scope.items[$scope.currentIndex].comments;
+            $scope.items[$scope.currentIndex].comments;
         }
 
         $scope.deleteItem = function (item) {
 
-            // localStorage.clear();
             let ind = $scope.items.indexOf(item);
-            
-            console.log(ind);
             $scope.items.splice(ind, 1);
             let len = $scope.items.length;
             Storage.setData($scope.items);
 
             if (len == 0) {
-                console.log("no");
                 $scope.currentIndex = -1;
                 $scope.disabledComment = true;
             } else {
                 $scope.currentIndex = ind-1; 
             }
-            
         } 
 
         $scope.createItem = function () {
             if (!checkForItems) {
                 $scope.currentIndex = 0;
             }
-            if($scope.textItem[0] != undefined ) {
+            if($scope.textItem != undefined ) {
                 $scope.items.push({username: $scope.textItem, comments: []} );
                 $scope.currentIndex = $scope.items.length - 1;
                 $scope.textItem = '';
@@ -86,12 +53,11 @@ module
               
                 Storage.setData($scope.items);
             }
-
         }
 
-        $scope.createComment = function (comment, e) {
+        $scope.createComment = function (e) {
             
-            if ($scope.textComment[0] != undefined) {
+            if ($scope.textComment != undefined) {
                 e = e || window.event;
                 
                 if (e.keyCode === 13) {
