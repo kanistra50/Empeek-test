00f87f96208fb47f05126976a214c9002bef0763
Nikolay Naumenko <naumenko.mykola@gmail.com>
2017-04-24 11:05:55 +0300

    Update controller.default.js

--- app/controllers/controller.default.js
+++ app/controllers/controller.default.js
@@ -9,13 +9,15 @@ module
         $scope.items = [];
         $scope.$index = 0;
         $scope.currentIndex = 0;
+        $scope.showItemAdvise = "none";
+        $scope.showCommentAdvise = "none";
         
-        let checkForItems = function () {
-            if ($scope.items != undefined || $scope.items[0] != -1) { 
-            return false; 
-            }
-            else {return true;}
-        }
+        // let checkForItems = function () {
+        //     if ($scope.items != undefined || $scope.items[0] != -1) {
+        //     return false;
+        //     }
+        //     else {return true;}
+        // };
 
         $scope.items = Storage.getData();
        
@@ -23,8 +25,14 @@ module
             $scope.textComment = '';
             $scope.disabledComment=false;
             $scope.currentIndex = $scope.items.indexOf(item);
-            $scope.items[$scope.currentIndex].comments;
-        }
+            // $scope.items[$scope.currentIndex].comments;
+        };
+
+        $scope.keypress = function(e){
+            if (e.keyCode === 13) {
+                $scope.createItem();
+            }
+        };
 
         $scope.deleteItem = function (item) {
 
@@ -39,35 +47,52 @@ module
             } else {
                 $scope.currentIndex = ind-1; 
             }
-        } 
+        };
 
-        $scope.createItem = function () {
-            if (!checkForItems) {
-                $scope.currentIndex = 0;
-            }
-            if($scope.textItem != undefined ) {
-                $scope.items.push({username: $scope.textItem, comments: []} );
+        $scope.createItem = function (clbckFn) {
+            // if (!checkForItems) {
+            //     $scope.currentIndex = 0;
+            // }
+
+            let el = $scope.items.find(e => e.username === $scope.textItem);
+
+            if($scope.textItem && !el) {
+                $scope.items.push({ username: $scope.textItem, comments: [] });
                 $scope.currentIndex = $scope.items.length - 1;
                 $scope.textItem = '';
-                $scope.disabledComment=false;
-              
+                $scope.disabledComment = false;
                 Storage.setData($scope.items);
+
+                // if (clbckFn && $scope.items.length == 4){
+                //     clbckFn($scope.items[0]);
+                // }
             }
-        }
+        };
 
         $scope.createComment = function (e) {
-            
-            if ($scope.textComment != undefined) {
-                e = e || window.event;
-                
-                if (e.keyCode === 13) {
-                    
-                    $scope.items[$scope.currentIndex].comments.push($scope.textComment);
+            $scope.showCommentAdvise = "none";
+            console.log($scope.textComment);
+            // e = e || window.event;
+            if (e.keyCode === 13) {
+
+                let commentsArr = $scope.items[$scope.currentIndex].comments;
+                let el = commentsArr.find(
+                    function check(element) {
+                        return element == $scope.textComment;
+                    });
+                console.log(el);  console.log(commentsArr);
+                //
+                //
+
+                if ($scope.textComment && !el) {
+
+                    commentsArr.push($scope.textComment);
                     $scope.textComment = '';
                     Storage.setData($scope.items);
-                };
+
+                } else {$scope.showCommentAdvise = "display";}
             }
-        }
+        };
        
         console.log("Controller loaded");
     }]);
